3bb7eefb96fb31de5e2958a36f283d9b87ddc59d
Oliko136 <olchick136@gmail.com>
2024-06-02 19:59:22 +0400

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,12 @@
+import { NavLink } from "react-router-dom";
 const HomePage = () => {
     return (
-        <h1>Home Page</h1>
+        <>
+            <h1>Welcome to Ukraine Camper Van Rentals!</h1>
+            <p>Discover the freedom of exploring Ukraine at your own pace with our top-quality camper vans. Whether you're planning a family adventure, a romantic getaway, or a solo expedition, we have the perfect vehicle for your journey. Enjoy the comfort, convenience, and flexibility of our well-equipped camper vans, and make unforgettable memories on the road. Start your adventure today!</p>
+            <NavLink to="/catalog">Explore catalog</NavLink>
+        </>
+        
     )
 }
 
