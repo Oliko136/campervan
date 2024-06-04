1c603d74126b35dfe823d4c6e59eecab8a4f6ae3
Oliko136 <olchick136@gmail.com>
2024-06-04 15:28:35 +0000

    Deploying to gh-pages from @ Oliko136/campervan@6511db87fa93224a5942d3a9511beab8338cecce 🚀

--- static/js/413.e244e6d8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[413],{306:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"Favorites Page"})}}}]);
-//# sourceMappingURL=413.e244e6d8.chunk.js.map
